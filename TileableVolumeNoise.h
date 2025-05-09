3300d92fe2f98d1cca736b20acb33bff321c7eb4
Mateusz <mateusz643@gmail.com>
2025-05-09 23:53:20 +0200

    xd

--- TileableVolumeNoise.h
+++ TileableVolumeNoise.h
@@ -5,11 +5,11 @@
 /// @return Tileable Worley noise value in [0, 1].
 /// @param p 3d coordinate in [0, 1], being the range of the repeatable pattern.
 /// @param cellCount the number of cell for the repetitive pattern.
-static float TileableWorleyNoise3D(float px, float py, float pz, float cellCount);
+float TileableWorleyNoise3D(float px, float py, float pz, float cellCount);
 
 /// @return Tileable Perlin noise value in [0, 1].
 /// @param p 3d coordinate in [0, 1], being the range of the repeatable pattern.
-static float TileablePerlinNoise3D(float px, float py, float pz, float frequency, int octaveCount);
+float TileablePerlinNoise3D(float px, float py, float pz, float frequency, int octaveCount);
 
 
 #endif // D_TILEABLE3DNOISE
